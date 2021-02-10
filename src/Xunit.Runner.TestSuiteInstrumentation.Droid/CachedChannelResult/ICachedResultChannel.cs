4206ae72da2ca368ea873ecae3d02438a2b3d987
Yauheni Puntus <yauheni_puntus@epam.com>
2021-02-10 18:19:52 +0300

    Add details about progress

---
+++ src/Xunit.Runner.TestSuiteInstrumentation.Droid/CachedChannelResult/ICachedResultChannel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Xunit.Runners.TestSuiteInstrumentation
+{
+    public interface ICachedResultChannel : IResultChannel
+    {
+        IReadOnlyCollection<TestResultViewModel> TestResults { get; }
+
+        void Clear();
+    }
+
+}
